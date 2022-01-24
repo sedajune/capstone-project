99c2bad435e8769aff8ff460ccfd115736969f3a
Silke Groenke <silkigr@gmail.com>
2022-01-24 14:50:23 +0100

    debugging again

--- read-file.js
+++ read-file.js
@@ -1,8 +1,6 @@
-//import { readFile } from "fs/promises";
+import { readFile } from "fs/promises";
 
-const fs = require("fs");
-
-fs.readFile("foo", "hello.json", (err, data) => {
+readFile("foo", "hello.json", (err, data) => {
   if (err) {
     console.error(err);
     return;
