132e8f9c4a8815eb3e197c612c5b9a8b693a87cb
Silke Groenke <silkigr@gmail.com>
2022-01-26 21:51:05 +0100

    components writing

--- create-react-component.js
+++ create-react-component.js
@@ -1,4 +1,23 @@
-import { mkdir } from "fs/promises";
+import { mkdir, writeFile } from "fs/promises";
+import path from "path";
+import process from "node:process";
+
+const jsxTemplate = (componentName) => `import React from 'react';
+
+const ${componentName} = ({children, ...rest}) => {
+	return <div {...rest}>{children}</div>
+}
+
+export default ${componentName};
+`;
+
+const readmeTemplate = (componentName) => `# ${componentName}
+
+## How to use this component
+
+................................................................
+
+`;
 
 // node create-react-component.js
 // npx create-react-app
@@ -9,11 +28,34 @@ import { mkdir } from "fs/promises";
 // ->
 // * out/atoms/Button/index.jsx
 // * out/atoms/Button/README.md
-
-// This function has to be called
-const createReactComponent = async () => {
+const outFolder = "out";
+// This function has to be called.
+const createReactComponent = async (directoryName, componentName) => {
+	const directory = path.join(outFolder, directoryName, componentName);
+	const jsxFileName = path.join(directory, "index.jsx");
+	const readmeFileName = path.join(directory, "README.md");
 	// We can use "await" since this is an async function
-	await mkdir("out/foo/bar");
-};
+	// CLI `mkdir -p`
+	await mkdir(directory, { recursive: true });
 
-void createReactComponent();
+	writeFile(jsxFileName, jsxTemplate(componentName))
+		.then(() => {
+			console.log(`Created file: ${jsxFileName}`);
+		})
+		.catch((error_) => {
+			console.error(error_);
+		});
+	writeFile(readmeFileName, readmeTemplate(componentName))
+		.then(() => {
+			console.log(`Created file: ${readmeFileName}`);
+		})
+
+		.catch((error_) => {
+			console.error(error_);
+		});
+};
+// Read arguments from CLI
+const [directoryName, componentName] = process.argv.slice(2);
+
+// Add arguments to the create function
+void createReactComponent(directoryName, componentName);
